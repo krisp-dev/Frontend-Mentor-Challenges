bbc964655397fed648f14c10356028b749bdf43f
Kris Pietrzak <krispdev@hotmail.com>
2021-07-16 18:41:03 +1000

    completed desktop layout

---
+++ Newbie/faq-accordion-card-main/accordion.js
@@ -0,0 +1,16 @@
+let faqButtons = document.getElementsByClassName('FAQ-item__button');
+
+for (let i = 0; i < faqButtons.length; i++) {
+    faqButtons[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+
+        let faqContent = this.nextElementSibling;
+
+        if (faqContent.style.maxHeight) {
+            faqContent.style.maxHeight = null;
+        }
+        else {
+            
+        }
+    });
+}
