1942e2e9a367fd9ce7459887226dd7e292fd9a61
Kris Pietrzak <krispdev@hotmail.com>
2021-12-21 00:21:28 +1000

    refactored js code

--- Junior/social-media-dashboard-with-theme-switcher-master/main.js
+++ Junior/social-media-dashboard-with-theme-switcher-master/main.js
@@ -1,5 +1,26 @@
-const checkbox = document.getElementById("checkbox");
+const themeButton = document.getElementById("checkbox");
+const darkTheme = "dark";
+const iconTheme = "bx-toggle-right";
 
-checkbox.addEventListener("change", () => {
-   document.body.classList.toggle("dark");
+// Previously selected theme
+const selectedTheme = localStorage.getItem("selected-theme");
+
+// Get current theme by validating the dark-theme class
+const getCurrentTheme = () =>
+   document.body.classList.contains(darkTheme) ? "dark" : "light";
+
+// Check if theme has been already selected
+if (selectedTheme) {
+   // Check if theme was activated or de-activated
+   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+      darkTheme
+   );
+}
+
+// Toggle the theme using toggle button
+themeButton.addEventListener("click", () => {
+   // Toggle theme & icon
+   document.body.classList.toggle(darkTheme);
+   // Save theme & icon to local storage
+   localStorage.setItem("selected-theme", getCurrentTheme());
 });
